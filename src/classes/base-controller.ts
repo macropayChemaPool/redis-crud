7084576080682f1f90d2e6cf2702e00ecc36d51a
Raul Chin Caamal <raul.chim@macropay.mx>
2024-01-05 11:52:06 -0600

    feat(back): implement base service

--- src/classes/base-controller.ts
+++
@@ -1,8 +0,0 @@
-import { RedisCacheHandler } from "./utils/redis-cache-handler";
-
-export class BaseController {
-  private readonly redisCacheHandler = new RedisCacheHandler()
-  onInit() {
-    console.log("Method to init")
-  }
-}
---
+++ src/classes/base-service.ts
@@ -0,0 +1,54 @@
+// import type { NextApiRequest } from 'next';
+import { RedisCacheHandler } from './utils/redis-cache-handler';
+
+type ServiceWithoutParams<T> = () => Promise<T>;
+// type ServiceWithRequest<T> = (req: NextApiRequest) => Promise<T>;
+// type ServiceWithRequestParams<T, S> = (
+//   req: NextApiRequest,
+//   params?: S
+// ) => Promise<T>;
+
+type ServiceFunction<T, S> = ServiceWithoutParams<T>;
+// | ServiceWithRequestParams<T, S>
+// | ServiceWithRequest<T>;
+
+interface IReturnEntity<T> {
+  /**
+   * sha (`SHA`) response is the key to set on your cookies
+   * 
+   * ***Example***
+   * ```js
+   * res.setHeader("Set-Cookie",`${YOUR_KEY}=${REDIS_KEY}; Secure; HttpOnly; SameSite=Lax; path=/`)
+   * ```
+   */
+  sha: string;
+  entities: T;
+}
+
+export class BaseService<T, S = unknown> {
+  private readonly redisCacheHandler = new RedisCacheHandler<T>();
+
+  constructor(private service: ServiceFunction<T, S>) {}
+
+  async getEntity(key: string): Promise<IReturnEntity<T>> {
+    const reply = await this.redisCacheHandler.getRedisState<T>(key);
+    console.log(reply)
+    if (reply) {
+      return {
+        sha: key,
+        entities: reply,
+      };
+    }
+
+    const entities = await this.service();
+    const REDIS_KEY = await this.redisCacheHandler.setRedisState({
+      body: entities,
+    });
+
+    return {
+      sha: REDIS_KEY,
+      entities,
+    };
+  }
+}
+
